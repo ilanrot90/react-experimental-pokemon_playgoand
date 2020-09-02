05d52daba0d88dd9fc8f6fbf3e16a479654f7ad7
ilanrot90 <30390232+ilanrot90@users.noreply.github.com>
2020-09-02 20:30:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<App />, rootElement);
 const rootElement = document.getElementById("root");
 ReactDOM.unstable_createRoot(rootElement).render(<App />);
